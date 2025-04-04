80ff2c44542ffb63d533a3bc2d1a8ccfa7786b4c
Hrigoriu Sharov <Hrigoriu@ukr.net>
2025-04-04 19:02:39 +0300

    Third commit: finish find and print the longest word

--- find_longest.py
+++ find_longest.py
@@ -1,10 +1,14 @@
 input_filename = "D:\\IT school\\Projects\\Git_learning\\Git_word\\PythonAI51PublicHForc\\input.txt"
-words = []
+longest_word = ''
 
 try:
     with open(input_filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            words.append(line.strip())
-    print('Прочитані слова:', words)
+        lines = file.readlines()
+        for line in lines:
+            current_word = line.strip()
+            if len(current_word) > len(longest_word):
+                longest_word = current_word
+    print(longest_word)
+
 except FileNotFoundError:
     print(f'Помилка: Файл "{input_filename}" не знайдено.')
