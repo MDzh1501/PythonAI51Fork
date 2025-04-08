2bfd818c6aa0cccbf8d3f1d1a4a37a41803f7df1
MDzh1501 <coolmarik1501@gmail.com>
2025-04-08 22:12:36 +0300

    Second commit: slight improvment of the existing algorithm

--- hw(dzhanhirov).py
+++ hw(dzhanhirov).py
@@ -1,5 +1,8 @@
 def find_the_longest(name):
-    with open(name, 'r', encoding='utf-8') as f:
-        return max((line.strip('\n') for line in f), key=len)
+    try:
+        with open(name, 'r', encoding='utf-8') as f:
+            return max((line.strip('\n') for line in f), key=len)
+    except FileNotFoundError:
+        print(f"Error! File {name} was not found!")
 
 print(find_the_longest('files/input.txt'))
